a9389de72750f0cb4b969f832db4898ac55eabe2
dengxiahei <49131949+922dnf@users.noreply.github.com>
2022-04-18 08:06:51 +0800

    Update get_ipv6.py

--- get_ipv6.py
+++ get_ipv6.py
@@ -3,8 +3,6 @@ pageURL='http://ipv6.ipv6-test.ch/ip/?callback=?'
 content=requests.get(pageURL).text.strip("callback")
 data = eval(content)
 print(data['ip'])
-data = eval(content.text)
-print(data['ip'])
 
 #获取ipv6的地址，用于ddns地址更新
 
